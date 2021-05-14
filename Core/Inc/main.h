6d2c7c6fceb62d8fe5af0cda61ec5047a14a70a5
SyntaxErr0r <SyntaxErr0r@gmail.com>
2021-05-14 22:50:33 +0200

    name the MAX6675 GPIO

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define MAX6685_NCS_Pin GPIO_PIN_4
+#define MAX6685_NCS_GPIO_Port GPIOE
 #define Green_LED_Pin GPIO_PIN_13
 #define Green_LED_GPIO_Port GPIOG
 /* USER CODE BEGIN Private defines */
