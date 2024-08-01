547971874ec473eea5c1539145921e98f4dfdbd9
SyntaxErr0r <SyntaxErr0r@gmail.com>
2024-08-01 15:03:41 +0200

    add missing usb init lines

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -275,7 +275,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -1164,7 +1164,7 @@ void App_Task(void *argument)
 
 	int heatOn = 0;
 	float ie = 0;
-
+	  MX_USB_DEVICE_Init();
   for(;;)
   {
     osDelay(500);
